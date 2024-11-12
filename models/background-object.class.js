97ca41fa6e9c289a7ac261f5f4f5587daf256b4b
Konstantin Stark <konstantin.stark.dev@gmail.com>
2024-11-12 11:14:12 +0100

    some fixes

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,5 +1,5 @@
 class BackgroundObject extends MovableObject {
-    width = 821;
+    width = 721;
     height = 480;
 
     constructor(imagePath, x) {
--- models/character.class.js
+++ models/character.class.js
@@ -40,10 +40,7 @@ class Character extends MovableObject {
 
         setInterval(() => {
             if (this.world.keyboard.D || this.world.keyboard.A) {
-                let i = this.currentImage % this.IMAGES_WALKING.length;
-                let path = this.IMAGES_WALKING[i];
-                this.img = this.imageCache[path];
-                this.currentImage++;
+                this.playAnimation(this.IMAGES_WALKING);
             }
         }, 50);
     }
