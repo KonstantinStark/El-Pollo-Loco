90a1ab02610015b2a5b5135b2233d877c0dbef68
Konstantin Stark <konstantin.stark.dev@gmail.com>
2024-11-13 09:17:49 +0100

    instanceof char chick

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -11,11 +11,8 @@ class Chicken extends MovableObject {
     constructor() {
         super().loadImage('./img/3_enemies_chicken/chicken_normal/1_walk/1_w.png')
         this.loadImages(this.IMAGES_WALKING);
-        
         this.x = 200 + Math.random() * 500;
-        this.speed = .15 + Math.random() * .5;
-       
-        
+        this.speed = .15 + Math.random() * .5;        
         this.animate();
     }
     animate() {
